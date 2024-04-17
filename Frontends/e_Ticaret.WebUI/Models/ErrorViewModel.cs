b54c2dfdfa3108fe8ae6d873ed9a33cf07445f9c
Bberkayyy <bberkayoguz@gmail.com>
2024-04-17 17:24:22 +0300

    UI Template eklendi.

---
+++ Frontends/e_Ticaret.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace e_Ticaret.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
