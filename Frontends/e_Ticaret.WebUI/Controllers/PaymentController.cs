c6171102f2657ff62394eebed26efeb80f32da7c
Bberkayyy <bberkayoguz@gmail.com>
2024-06-10 10:45:49 +0300

    Ödeme işlemleri kart ile ödeme için sayfa credit cart script ile oluşturuldu. Payment mikroservisi ve ui için işlemler daha sonra yapılacak.

---
+++ Frontends/e_Ticaret.WebUI/Controllers/PaymentController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace e_Ticaret.WebUI.Controllers;
+
+public class PaymentController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
